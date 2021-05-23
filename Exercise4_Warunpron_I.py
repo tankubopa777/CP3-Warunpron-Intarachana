08f229ebc311ae5972d391c7929c0a428a9cdcdd
tankubopa777 <84582195+tankubopa777@users.noreply.github.com>
2021-05-24 00:53:06 +0700

    New

---
+++ Exercise4_Warunpron_I.py
@@ -0,0 +1,15 @@
+"""
+FE คือ คะแนนของ Foundation English
+GB คือ คะแนนของ General Business
+ICS คือ คะแนนของ Introduction to Computer Systems
+CP คือ คะแนนของ Computer Programming
+"""
+FE = 48.6
+GB = 51.9
+ICS = 68.7
+CP = 57
+print("---Your Score---")
+print("Foundation English :",FE)
+print("General Business :",GB)
+print("Introduction to Computer Systems :",ICS)
+print("Computer Programming :",CP)
