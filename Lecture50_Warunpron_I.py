2af3d5333f6457d98333c5a80779a303abcf7b79
tankubopa777 <84582195+tankubopa777@users.noreply.github.com>
2021-05-29 14:25:51 +0700

    Update Lecture50_Warunpron_I.py

---
+++ Lecture50_Warunpron_I.py
@@ -0,0 +1,14 @@
+def บวกเลข(x,y):
+    print(x+y)
+def ลบเลข(x,y):
+    print(x-y)
+def คูณเลข(x,y):
+    print(x*y)
+def หารเลข(x,y):
+    print(x/y)
+x = int(input("ใส่ค่า x :"))
+y = int(input("ใส่ค่า y :"))
+print(บวกเลข(x,y))
+print(ลบเลข(x,y))
+print(คูณเลข(x,y))
+print(หารเลข(x,y))
